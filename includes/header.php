9986b7d941d34df05bb183932d5ee1c407ac2325
Theo-Tim Gonzales <theo-tim.gonzales@laplateforme.io>
2025-02-19 16:27:54 +0100

    addition of two images in the img folder/ addition of the index that presents the guest book/ addition of the footer/ addition of the header with images/links that lead to the different pages/ addition of the css in the style.css folder for the index/header/footer with responsives

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,36 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livre d'or</title>
-    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+
+<?php 
+$current_page = basename($_SERVER['PHP_SELF']); // Récupère le fichier actuel
+?>
+
 <header>
-    <nav class=barrenav>
-        <a href="index.php">Accueil</a>
-        <a href="livre-or.php">Livre d'or</a>
+    <nav class="barrenav">
+        <a href="index.php" class="<?= ($current_page == 'index.php') ? 'active' : '' ?>">
+            <img src="../LIVRE-OR/assets/img/home.svg" alt="home">
+        </a>
+        <a href="livre-or.php" class="<?= ($current_page == 'livre-or.php') ? 'active' : '' ?>">
+            <img src="../LIVRE-OR/assets/img/book.svg" alt="book">
+        </a>
         <?php if (isset($_SESSION['user_id'])): ?>
-            <a href="profil.php">Profil</a>
-            <a href="commentaire.php">Ajouter un commentaire</a>
-            <a href="logout.php">Déconnexion</a>
+            <a href="profil.php" class="<?= ($current_page == 'profil.php') ? 'active' : '' ?>">
+                <img src="../LIVRE-OR/assets/img/profile.svg" alt="profile">
+            </a>
         <?php else: ?>
-            <a href="inscription.php">Inscription</a>
-            <a href="connexion.php">Connexion</a>
+            <a href="connection.php" class="<?= ($current_page == 'connection.php') ? 'active' : '' ?>">
+                <img src="../LIVRE-OR/assets/img/profile.svg" alt="profile">
+            </a>
         <?php endif; ?>
     </nav>
 </header>
+
+</body>
+</html>
