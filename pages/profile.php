4550baf06a45fc95610296e4261bc44acdb93640
thomas-robert <thomas.robert@laplateforme.io>
2025-02-20 15:15:15 +0100

    css profile

--- pages/profile.php
+++ pages/profile.php
@@ -19,7 +19,7 @@ if (!$currentUser) {
     exit();
 }
 
- // Update session if login is changed
+// Update session if login is changed
 $message = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $newLogin = !empty($_POST['login']) ? $_POST['login'] : null;
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($newLogin || $newPassword) {
         $message = $user->updateUser($_SESSION['user'], $newLogin, $newPassword);
-       // Update session if login is changed
+        // Update session if login is changed
         if ($newLogin) {
             $_SESSION['login'] = $newLogin;
         }
@@ -39,29 +39,46 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style-profile.css">
+    <link rel="stylesheet" href="../assets/css/style-commun.css">
+
     <title>Profil</title>
 </head>
+
 <body>
-    <h2>Modifier mon profil</h2>
-    
-    <?php if (!empty($message)) : ?>
-        <p><?= $message ?></p>
-    <?php endif; ?>
+    <?php include '../includes/header.php'; ?>
 
-    <form method="POST">
-        <label for="login">Nouveau Login :</label>
-        <input type="text" name="login" id="login" value="<?= htmlspecialchars($currentUser['login']) ?>">
-        
-        <label for="password">Nouveau Mot de Passe :</label>
-        <input type="password" name="password" id="password">
+    <main class="main">
+        <div class="title-bloc">
+            <img src="../assets/img/leftlogo.png" alt="logo">
+            <h1 class="title-h1">Modifier mon profil</h1>
+            <img src="../assets/img/rightlogo.png" alt="logo">
+        </div>
 
-        <button type="submit">Modifier</button>
-    </form>
+        <?php if (!empty($message)) : ?>
+            <p><?= $message ?></p>
+        <?php endif; ?>
+        <section class="profile-bloc">
+            <form method="POST">
+                <label for="login">Nouveau Login :</label>
+                <input type="text" name="login" id="login" value="" class="input" <?= htmlspecialchars($currentUser['login']) ?>">
 
-    <a href="/logout.php">Se déconnecter</a>
+                <label for="password">Nouveau Mot de Passe :</label>
+                <input type="password" name="password" id="password" class="input">
+
+                <button type="submit" class="button">Modifier</button>
+            </form>
+
+            <a href="/logout.php" class="button-logout">Se déconnecter</a>
+        </section>
+        <?php include '../includes/footer.php'; ?>
+    </main>
+
 </body>
+
 </html>
